3c73a52689e3204a7123e0421c23cf51fe7f5d40
JEBASS <jebas7@outlook.com>
2026-07-02 12:23:09 -0300

    desafios

---
+++ PythonExercícios/ex010.py
@@ -0,0 +1,4 @@
+r = float(input('Digite o valor em Reais? R$'))
+d = r / 3.27
+print('Com R${:.2f} você pode comprar US${:.2f}'.format(r, d))
+
--- PythonTeste/aula07a.py
+++ PythonTeste/aula07a.py
@@ -5,6 +5,5 @@ m = n1 * n2
 d = n1 / n2
 di = n1 // n2
 e = n1 ** n2
-
-print('A soma é {}, o produto é {} e a divisão é {}'.format(s, m, d))
-print('Divisão inteira {} e potência {}'.format(di, e))
+print('A soma é {} \nO produto é {} \nA divisão é {:.3f}\n'.format(s, m, d), end='')
+print('Divisão inteira {} \nA potência {}'.format(di, e))
