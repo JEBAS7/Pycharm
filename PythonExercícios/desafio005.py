4b661d7b33fd78c4771af0b384321d4d95d67486
JEBASS <jebas7@outlook.com>
2026-07-02 12:23:09 -0300

    desafios

---
+++ PythonExercícios/desafio005.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número:'))
+s = n + 1
+a = n - 1
+print('O número sucessor é {}'.format(s))
+print('O número antecessor é {}'.format(a))
---
+++ PythonExercícios/desafio011.py
@@ -0,0 +1,5 @@
+l = float(input('Qual a largura da parede? '))
+a = float(input('Qual a altura da parede? '))
+c = l * a
+qt = c / 2
+print('É necessário {} litros de tinta'.format(qt))
