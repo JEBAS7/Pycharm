6557e8cdceb150349334e7d95ab026b962778188
JEBASS <jebas7@outlook.com>
2026-07-04 10:23:42 -0300

    ex021

--- PyCharmExercícios/ex021.py
+++ PyCharmExercícios/ex021.py
@@ -2,13 +2,13 @@ import pygame
 import time
 pygame.init()
 pygame.mixer.init()
-pygame.mixer.music.load('musica.mp3')
+pygame.mixer.music.load('ex021.mp3')
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy():
     time.sleep(1)
 #abaixo codigo da aula não funciona mais
 '''import pygame
 pygame.init()
-pygame.mixer.music.load('musica.mp3')
+pygame.mixer.music.load('ex021.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()'''
