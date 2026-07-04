20178bdc8b12e3631807851f8920b0164c806cd9
JEBASS <jebas7@outlook.com>
2026-07-04 20:29:42 -0300

    Exercícios-aula08a

---
+++ PythonTeste/aula08a.py
@@ -0,0 +1,6 @@
+#import math
+import emoji
+#num = int(input('digite um numero: '))
+#raiz = math.sqrt(num)
+#print('A raiz de {} é igual a {:.2f}'.format(num, raiz))
+print(emoji.emojize("Olá, Mundo :earth_americas:", language='alias'))
