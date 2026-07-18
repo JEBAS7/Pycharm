b79623ccb9ac86b2d37d6525ae7137e0bb1a49e2
JEBASS <jebas7@outlook.com>
2026-07-18 02:07:21 -0300

    Exercícios-aula18

--- PyCharmExercícios/ex086.py
+++ PyCharmExercícios/ex086.py
@@ -4,7 +4,6 @@ matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 for l in range(0, 3):
     for c in range(0, 3):
         matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
-
 print('-=' * 30)
 
 # Exibição inteligente (Substitui os seus 3 prints manuais)
