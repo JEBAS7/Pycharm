fdd5cef92edf2b9ef499769b2baadeb9b70558ae
JEBASS <jebas7@outlook.com>
2026-07-22 15:16:29 -0300

    Exercícios-aula19

--- PyCharmExercícios/ex090.py
+++ PyCharmExercícios/ex090.py
@@ -1,10 +1,13 @@
 aluno = {}
 aluno['nome'] = str(input('Nome: '))
 aluno['media'] = float(input(f'Média de {aluno['nome']}: '))
-print(f'Nome é igual a {aluno['nome']}')
-print(f'Média é igual a {aluno['media']}')
 if aluno['media'] >= 7:
-    print('Situação é igual a Aprovado!')
+    aluno['situação'] = 'aprovado'
+elif 5 <= aluno['media'] < 7:
+    aluno['situação'] = 'recuperação'
 else:
-    print('Situação é igual a Reprovado!')
+    aluno['situação'] = 'reprovado'
+print('-' * 40)
+for k, v in aluno.items():
+    print(f'    - {k} é igual a {v}')
     
