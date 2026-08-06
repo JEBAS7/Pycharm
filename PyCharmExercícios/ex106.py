2c44f5da67be77c593c7906042acfe03e7f35b8a
JEBAS7 <jebas7@outlook.com>
2026-08-05 23:07:37 -0300

    Exercícios_aula21

--- PyCharmExercícios/ex106.py
+++ PyCharmExercícios/ex106.py
@@ -1,4 +1,46 @@
-def Pyhelp(txt):
-    print('~' * len(txt))
-    print(f'      {txt}')
-    print('~' * len(txt))
+import sys
+
+
+def biblioteca(txt):
+    tamanho = len(txt)
+
+    while True:
+        # Ativa fundo preto e texto verde para todo o bloco
+        sys.stdout.write('\033[1;32;40m')
+        sys.stdout.flush()
+
+        # Cabeçalho do PyHELP
+        print(f'{"~" * tamanho}')
+        print(f'{txt}')
+        print(f'{"~" * tamanho}')
+
+        # ESTRATÉGIA PARA O PYCHARM: Exibe a mensagem como print normal (mantém fundo preto)
+        print("Função ou Biblioteca (ou 'fim' para sair) > ", end='')
+
+        # O input fica vazio, limitando a barra cinza do PyCharm apenas ao cursor de digitação
+        comando = str(input()).strip()
+
+        # Verifica a condição de parada antes do help
+        if comando.upper() == 'FIM':
+            print(f'{"~" * tamanho}')
+            break
+
+        # Linha decorativa antes do help
+        print(f'{"~" * tamanho}')
+
+        # Executa a ajuda (que herda perfeitamente o verde e preto)
+        help(comando)
+
+        # Linha de fechamento do bloco de ajuda
+        print(f'{"~" * tamanho}\n')
+
+    # Mensagem final totalmente envelopada na estética verde/preto
+    print("PROGRAMA ENCERRADO. ATÉ LOGO!")
+    print(f'{"~" * tamanho}')
+
+    # Reseta o terminal completamente apenas após fechar tudo
+    print('\033[0m', end='')
+
+
+# Execução do sistema
+biblioteca('        SISTEMA DE AJUDA PyHELP         ')
